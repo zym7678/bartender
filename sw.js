63c9e46604992adef37a582aa8d8eca6c33be018
张越铭 <3329910006@qq.com>
2026-06-16 00:30:12 +0800

    🎉 初始提交：调酒库 PWA

---
+++ sw.js
@@ -0,0 +1,37 @@
+﻿const CACHE_NAME = "cocktail-bar-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./cabinet.html",
+  "./recipes.html",
+  "./shake.html",
+  "./make.html",
+  "./log.html",
+  "./style.css",
+  "./common.js",
+  "./manifest.json",
+  "./icons/icon-192.svg",
+  "./icons/icon-512.svg"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
